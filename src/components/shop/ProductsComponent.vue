2bc097c45232ad4cc1398d95df69f1f2ffbe0a01
TiannaLopes <tiannaalinalopes@gmail.com>
2024-03-18 20:40:57 -0400

    Recent changes

---
+++ src/components/shop/ProductsComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="product in products" :key="product.id">{{ product.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import PrintfulService from '@/services/printfulService';
+
+export default {
+  data() {
+    return {
+      products: []
+    };
+  },
+  async mounted() {
+    try {
+      const response = await PrintfulService.getProducts();
+      this.products = response.data;
+    } catch (error) {
+      console.error("There was an issue fetching the products:", error);
+    }
+  }
+};
+</script>
